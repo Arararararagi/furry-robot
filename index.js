dcf03df59e77b1ec313a3e075a1d699feaa33a24
teclas <teclas@gmx.com>
2013-08-05 17:46:59 +0100

    Added Annuity

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
-/*window.onload = function() {
-    $("input").keyup(function () {
-      var value = $(this).val();
-      $("#PV").text(value);
-    }).keyup();
-};*/
-
 window.onload = function() {
+	//Perpetuity
+
+	$("#Capitalper").keyup(function () {
+		$("#interestper").keyup(function () {
+			var capital = $("#Capitalper").val();
+			var interest = $("#interestper").val();
+			$("#PVperpetuity").text((capital/interest)*100);
+		})
+	}).keyup();
+	$("#interestper").keyup(function () {
+		$("#Capitalper").keyup(function () {
+			var capital = $("#Capitalper").val();
+			var interest = $("#interestper").val();
+			$("#PVperpetuity").text(((capital/interest)*100));
+		})
+	}).keyup();
+
+/*
+	//Annuity
+
 	$("#Capital").keyup(function () {
 		$("#interest").keyup(function () {
-			var capital = $("#Capital").val();
-			var interest = $("#interest").val();
-			$("#PV").text(capital/interest);
+			$("#periods").keyup(function () {
+				var periods = $("#periods").val();
+				var capital = $("#Capital").val();
+				var interest = $("#interest").val();
+				$("#PVannuity").text((capital/interest)*(1-(1/Math.pow(1+interest, periods))));
+			})
 		})
 	}).keyup();
 	$("#interest").keyup(function () {
 		$("#Capital").keyup(function () {
-			var capital = $("#Capital").val();
-			var interest = $("#interest").val();
-			$("#PV").text(capital/interest);
-		})
+			$("#periods").keyup(function () {
+				var periods = $("#periods").val();
+				var capital = $("#Capital").val();
+				var interest = $("#interest").val();
+				$("#PVannuity").text((capital/interest)*(1-(1/Math.pow(1+interest, periods))));
+			})
 	}).keyup();
+*/
+
+	$('input[type="number"]').focus(function() {
+    $(this).addClass("focus");
+
+
+});
+ 
+$('input[type="number"]').blur(function() {
+    $(this).removeClass("focus");
+});
 }
