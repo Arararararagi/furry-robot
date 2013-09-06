fa1621414a1d4952d6f90dd8554d0aca084d7826
teclas <teclas@gmx.com>
2013-09-06 20:47:54 +0100

    Indentation, text, new home

--- index.js
+++ index.js
@@ -1,71 +1,74 @@
 var objPage = null;
+
 function showPage(pageName) {
-if (objPage) objPage.style.display = 'none';
-	objPage=document.getElementById(pageName);
-objPage.style.display = 'block';
+    if (objPage) objPage.style.display = 'none';
+    objPage = document.getElementById(pageName);
+    objPage.style.display = 'block';
 };
 
+
 window.onload = function() {
-	$('input[type="number"]').focus(function() {
-    	$(this).addClass("focus");
+    $('input[type="number"]').focus(function() {
+        $(this).addClass("focus");
     });
 
-	$('input[type="number"]').blur(function() {
-	    $(this).removeClass("focus");
-	});
+    $('input[type="number"]').blur(function() {
+        $(this).removeClass("focus");
+    });
 
-	showPage("home");
+    showPage("home");
 
-	/*//Perpetuity
+    if (navigator.userAgent.indexOf('MSIE') != -1 || navigator.userAgent.indexOf('Webkit') != -1) {
+        // Using IE
+        window.location = "./browser.html"
+    };
 
-	$("#Capitalper").keyup(function () {
-		$("#interestper").keyup(function () {
-			var capital = $("#Capitalper").val();
-			var interest = $("#interestper").val();
-			$("#PVperpetuity").text((capital/interest)*100);
-		})
-	}).keyup();
-	$("#interestper").keyup(function () {
-		$("#Capitalper").keyup(function () {
-			var capital = $("#Capitalper").val();
-			var interest = $("#interestper").val();
-			$("#PVperpetuity").text(((capital/interest)*100));
-		})
-	}).keyup();*/
-	("$perpetuity").key(function(){
-			var capital = $("#Capitalper").val();
-			var interest = $("#interestper").val();
-			$("#PVperpetuity").text((capital/interest)*100);
-	}).change();
+    //Perpetuity
 
-	//Annuity
+    $("#Capitalper").keyup(function() {
+        $("#interestper").keyup(function() {
+            var capital = $("#Capitalper").val();
+            var interest = $("#interestper").val();
+            $("#PVperpetuity").text((capital / interest) * 100);
+        })
+    }).keyup();
 
-	$("#button").click(function(){
-		var periods = $("#periodsann").val();
-		var capital = $("#Capitalann").val();
-		var interest = $("#interestann").val();
-		$("#PVannuity").text((capital/(interest/100))*(1-(1/Math.pow(1+(interest/100),periods))));
+    $("#interestper").keyup(function() {
+        $("#Capitalper").keyup(function() {
+            var capital = $("#Capitalper").val();
+            var interest = $("#interestper").val();
+            $("#PVperpetuity").text(((capital / interest) * 100));
+        })
+    }).keyup();
+
+    //Annuity
+
+    $("#button").click(function() {
+        var periods = $("#periodsann").val();
+        var capital = $("#Capitalann").val();
+        var interest = $("#interestann").val();
+        $("#PVannuity").text((capital / (interest / 100)) * (1 - (1 / Math.pow(1 + (interest / 100), periods))));
 
 
-});
- 
- 	//Growing Annuity
+    });
 
- 	$("#button1").click(function(){
- 		var periods = $("#periodsgann").val();
- 		var capital = $("#Capitalgann").val();
- 		var interest = $("#interestgann").val();
- 		var growth = $("#growthgann").val();
- 		$("#PVannuityg").text((capital/((interest-growth)/100))*(1-Math.pow((1+growth/100)/(1+interest/100),periods)));
- 	});
+    //Growing Annuity
 
- 	//Growing Perpetuity
+    $("#button1").click(function() {
+        var periods = $("#periodsgann").val();
+        var capital = $("#Capitalgann").val();
+        var interest = $("#interestgann").val();
+        var growth = $("#growthgann").val();
+        $("#PVannuityg").text((capital / ((interest - growth) / 100)) * (1 - Math.pow((1 + growth / 100) / (1 + interest / 100), periods)));
+    });
 
- 	$("#button2").click(function(){
- 		var capital = $("#Capitalgper").val();
- 		var interest = $("#interestgper").val();
- 		var growth = $("#growthgper").val();
- 		$("#PVperpetuityg").text(capital/((interest-growth)/100));
- 	});
+    //Growing Perpetuity
+
+    $("#button2").click(function() {
+        var capital = $("#Capitalgper").val();
+        var interest = $("#interestgper").val();
+        var growth = $("#growthgper").val();
+        $("#PVperpetuityg").text(capital / ((interest - growth) / 100));
+    });
 
 }
